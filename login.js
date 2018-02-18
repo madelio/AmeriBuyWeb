9bc9af39a27ab32362a86f6f3dd5ba538fdf1739
ZJ Teoh <zteoh@ucsd.edu>
2018-02-18 13:30:58 -0800

    Add files via upload

--- login.js
+++ login.js
@@ -1,13 +1,24 @@
 		function login() {
 			var email = document.getElementById('email').value;
 			var pass = document.getElementById('pass').value;
- 
-			var _email = localStorage.getItem("email");
-			var _pass = localStorage.getItem("pass");
 
-			if(email == _email && pass == _pass) {
-				document.location.href = "file:///Users/zjteoh/Desktop/AmeriBuyWeb-master-2/home.html";
-				localStorage.setItem("state", "logged-in");
+			if(email == '' || pass == '') {
+				document.getElementById('errormsg').classList.remove('hide');
 			}
+ 			else {
+	 			document.getElementById('errormsg').classList.add('hide');
+
+				var _email = localStorage.getItem("email");
+				var _pass = localStorage.getItem("pass");
+
+				if(email == _email && pass == _pass) {
+					document.getElementById('errormsg2').classList.add('hide');
+					localStorage.setItem("state", "logged-in");
+					document.location.href = "./home.html";
+				}
+				else {
+					document.getElementById('errormsg2').classList.remove('hide');
+				}
+			}	
 
 		}
--- signup.js
+++ signup.js
@@ -1,6 +1,4 @@
 		function setUserValues() {
-
-
 			var email = document.getElementById('email').value;
 			var pass = document.getElementById('pass').value;
 			var fname = document.getElementById('fname').value;
@@ -8,13 +6,17 @@
 			var mnum = document.getElementById('mnum').value;
 			var addr = document.getElementById('addr').value;
 
-			localStorage.setItem("email", email);
-			localStorage.setItem("pass", pass);
-			localStorage.setItem("fname", fname);
-			localStorage.setItem("lname", lname);
-			localStorage.setItem("mnum", mnum);
-			localStorage.setItem("mnum", addr);
-
-
-			document.location.href = "file:///Users/zjteoh/Desktop/AmeriBuyWeb-master-2/home.html";
+			if(email == '' || pass == '' || fname == '' || lname == '' || mnum == '' || addr == '') {
+				document.getElementById('errormsg').classList.remove('hide');
+			}
+			else {
+				localStorage.setItem("email", email);
+				localStorage.setItem("pass", pass);
+				localStorage.setItem("fname", fname);
+				localStorage.setItem("lname", lname);
+				localStorage.setItem("mnum", mnum);
+				localStorage.setItem("mnum", addr);
+				document.getElementById('errormsg').classList.add('hide');
+				document.location.href = "./home.html";
+			}
 		}
